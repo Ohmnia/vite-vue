6a3caf46857f765e700f2fc3254b69412d4cdeec
Ohmnia <106306590+Ohmnia@users.noreply.github.com>
2022-12-31 22:41:35 +0700

    1 file modified, 1 added, 1 deleted

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import javascriptLogo from './public/javascript.svg'
 import viteLogo from './public/vite.svg'
+
+import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
